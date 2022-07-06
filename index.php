290e4601be8d447e0a1509880598a26de2bc0072
nirva <jvaldes381@alumnos.cftmaule.cl>
2022-07-06 19:55:44 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login de ingreso</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <form  action="validar.php" method="POST" >
+         <h1>Ingreso</h1>
+            <p>usuario</p>
+            <input type="text" placeholder="ingrese un usuario" name="user">
+                 <p>password</p>
+                 <input type="password" placeholder="ingrese una password" name="pass"><p></p>
+                     <input type="submit"  value="Ingresar">
+                     <a href="registro.php" title="registro" del enlace”> texto ancla</a>
+
+    </form>
+</body>
+</html>
